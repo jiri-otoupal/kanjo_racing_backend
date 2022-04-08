ec159090228018b4932506b7518fcb2fa2da465b
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2022-04-08 19:46:23 +0200

    optimized

--- db/models/Race.php
+++ db/models/Race.php
@@ -48,7 +48,7 @@ class Race extends DB
                                                                  ('$escaped_raceId','$escaped_step','$escaped_lat','$escaped_lng')");
     }
 
-    public function add($rid, $name, $start_time, $lat, $lng, $owner_id, $min_r, $max_r, $max_hp, $password, $heat_grade, $min_karma, $chat_link, $img_url = null)
+    public function bindForEdit($query_string, $type_string, $rid, $name, $start_time, $lat, $lng, $owner_id, $min_r, $max_r, $max_hp, $password, $heat_grade, $min_karma, $chat_link, $img_url = null, $where_id = null)
     {
         $escaped_rid = $this->escape($rid);
         $escaped_name = $this->escape($name);
@@ -65,46 +65,34 @@ class Race extends DB
         $escaped_chat_link = $this->escape($chat_link);
         $escaped_img_url = $this->escape($img_url);
 
-        $prepared = $this->connection->prepare("INSERT INTO race
-        VALUES 
-               (?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-        $prepared->bind_param("issddiiiississ", $escaped_rid, $escaped_name, $escaped_start_time, $escaped_lat, $escaped_lng, $escaped_owner_id,
+        $prepared = $this->connection->prepare($query_string);
+        $prepared->bind_param($type_string, $escaped_rid, $escaped_name, $escaped_start_time, $escaped_lat, $escaped_lng, $escaped_owner_id,
             $escaped_min_r, $escaped_max_r, $escaped_max_hp, $escaped_password, $escaped_heat_grade,
-            $escaped_min_karma, $escaped_chat_link, $escaped_img_url);
-
+            $escaped_min_karma, $escaped_chat_link, $escaped_img_url, $where_id);
         return $prepared->execute();
+    }
+
+    public function add($rid, $name, $start_time, $lat, $lng, $owner_id, $min_r, $max_r, $max_hp, $password, $heat_grade, $min_karma, $chat_link, $img_url = null)
+    {
+        $query_string = "INSERT INTO race
+        VALUES 
+               (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $types = "issddiiiississ";
+        return $this->bindForEdit($query_string, $types, $rid, $name, $start_time, $lat, $lng, $owner_id, $min_r, $max_r, $max_hp, $password, $heat_grade, $min_karma, $chat_link, $img_url);
     }
 
     public function modifyRace($race_id, $name, $start_time, $lat, $lng, $owner_id, $min_r, $max_r, $max_hp, $password, $heat_grade, $min_karma, $chat_link, $img_url = null)
     {
-        //TODO: Merge duplicity
-        $escaped_rid = $this->escape($race_id);
-        $escaped_name = $this->escape($name);
-        $escaped_start_time = $this->escape($start_time);
-        $escaped_lat = $this->escape($lat);
-        $escaped_lng = $this->escape($lng);
-        $escaped_owner_id = $this->escape($owner_id);
-        $escaped_min_r = $this->escape($min_r);
-        $escaped_max_r = $this->escape($max_r);
-        $escaped_max_hp = $this->escape($max_hp);
-        $escaped_password = $this->escape($password);
-        $escaped_heat_grade = $this->escape($heat_grade);
-        $escaped_min_karma = $this->escape($min_karma);
-        $escaped_chat_link = $this->escape($chat_link);
-        $escaped_img_url = $this->escape($img_url);
-
-        $prepared = $this->connection->prepare("UPDATE race SET  
+        $query_string = "UPDATE race SET  
                 race_id=?,name=?,start_time=?,
                 latitude=?,longitude=?,owner_id=?,min_racers=?,
                              max_racers=?,max_hp=?,password=?,
                              heat_grade=?,min_req_karma=?,
                              chat_link=?,img_url=?
-             WHERE race_id=?");
-        $prepared->bind_param("issddiiiississi", $escaped_rid, $escaped_name, $escaped_start_time, $escaped_lat, $escaped_lng, $escaped_owner_id,
-            $escaped_min_r, $escaped_max_r, $escaped_max_hp, $escaped_password, $escaped_heat_grade,
-            $escaped_min_karma, $escaped_chat_link, $escaped_img_url, $escaped_rid);
+             WHERE race_id=?";
+        $types = "issddiiiississi";
 
-        return $prepared->execute();
+        return $this->bindForEdit($query_string, $types, $race_id, $name, $start_time, $lat, $lng, $owner_id, $min_r, $max_r, $max_hp, $password, $heat_grade, $min_karma, $chat_link, $img_url, $race_id);
     }
 
     public function deleteRace($id)
